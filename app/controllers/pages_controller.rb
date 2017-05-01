0ee8db97c372938dbcf1c0c3e6ccd6b744c61976
Gregoire Bollore <gregoire.bollore@outlook.fr>
2017-05-01 22:26:11 +0100

    Homepage & modals

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_family!, only: [ :home ]
 
+  helper_method :resource_name, :resource, :devise_mapping
+
   def home
+    @son = Son.new
   end
+
+  def resource_name
+    :family
+  end
+
+  def resource
+    @resource ||= Family.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:family]
+  end
+
 end
