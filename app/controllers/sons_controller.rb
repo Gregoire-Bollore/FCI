65b4df4ac9187b44295d543d8e4cddde1d700d13
Gregoire Bollore <gregoire.bollore@outlook.fr>
2017-12-10 18:14:33 +0100

    Alert messages & personal page

--- app/controllers/sons_controller.rb
+++ app/controllers/sons_controller.rb
@@ -3,5 +3,11 @@ class SonsController < ApplicationController
 
   def show
     @son = current_son
+    # if Sponsorship.exists?(son_id: @son.id)
+    #   @family_number = Sponsorship.where(son_id: @son.id).count
+    #   (1..@family_number).each do |n|
+    #     @family_"#{n}" = Sponsorship.where(son_id: @son.id)["#{n}"-1]
+    #   end
+    # end
   end
 end
