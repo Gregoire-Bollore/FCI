67fd4aa00e2c2e3b1b539d756d561574149c6b33
Gregoire Bollore <gregoire.bollore@outlook.fr>
2018-03-14 22:10:50 +0100

    Morning and evening prayers

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get 'team', to: 'pages#team'
   get 'sons_prayer', to: 'pages#sons_prayer'
   get 'families_prayer', to: 'pages#families_prayer'
+  get 'morning_prayer', to: 'pages#morning_prayer'
+  get 'evening_prayer', to: 'pages#evening_prayer'
 end
