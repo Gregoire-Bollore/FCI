a059a6c88117a3f3d9393315bf880ca50d807a62
Gregoire Bollore <gregoire.bollore@outlook.fr>
2018-06-17 16:56:11 +0200

    Activeadmin

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -6,7 +6,8 @@ class Sponsorship < ActiveRecord::Base
     @sponsorship_family = Family.where(id: self.family_id).first
     @sponsorship_son = Son.where(id: self.son_id).first
     if
-      return 'Famille  - ' + @sponsorship_family.last_name + " || " + @sponsorship_son.level + " - " + @sponsorship_son.last_name
+      dreturn 'Famille  - ' + @sponsorship_family.last_name + " || " + @sponsorship_son.level + " - " + @sponsorship_son.last_name
     else
+    end
   end
 end
